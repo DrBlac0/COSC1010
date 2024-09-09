2bdf368790b5709829511e8b865cf30cae123c19
Andres Hunter <AndresHunter@westernwyoming.edu>
2024-09-08 19:02:59 -0600

    Add files via upload

---
+++ Sales-Prediction/Sales-Prediction/# Get the predicted total sales.py
@@ -0,0 +1,6 @@
+# Get the predicted total sales
+total_sales = float(input('Enter the projected sales:'))
+# Calculate the profit
+profit = total_sales * 0.23
+# Display the profit
+print("The Profit is $:", format(profit,',.2f'))
